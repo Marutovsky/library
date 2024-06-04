438693e0e03a22431894908d4ace0daeef7e858e
marutovsky <33104282+Marutovsky@users.noreply.github.com>
2024-06-04 13:07:49 +0200

    Move all dialog related variables into object

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const content = document.querySelector('.content');
 const newBookButton = document.querySelector('#new-book');
-const newBookDialog = document.querySelector('#new-book-dialog');
-const addBookButton = document.querySelector('#add-book-btn');
-const dialogCloseButton = document.querySelector('#close-dialog');
-const dialogTitleInput = document.querySelector('#dialog-title');
-const dialogAuthorInput = document.querySelector('#dialog-author');
-const dialogPagesInput = document.querySelector('#dialog-pages');
-const dialogReadYes = document.querySelector('#dialog-read-yes');
-const dialogReadNo = document.querySelector('#dialog-read-no');
-let dialogReadChecked = document.querySelector('input[name="read"]:checked');;
+
+const dialog = {
+  newBook: document.querySelector('#new-book-dialog'),
+  addBookButton: document.querySelector('#add-book-btn'),
+  closeButton: document.querySelector('#close-dialog'),
+  titleInput: document.querySelector('#dialog-title'),
+  authorInput: document.querySelector('#dialog-author'),
+  pagesInput: document.querySelector('#dialog-pages'),
+  readYes: document.querySelector('#dialog-read-yes'),
+  readNo: document.querySelector('#dialog-read-no'),
+  readChecked: document.querySelector('input[name="read"]:checked')
+};
 
 let myLibrary = [
   {title: 'The Hobbit: An Unexpected Journey', author: 'Rowling', pages: '123', read: false},
@@ -25,10 +28,10 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  let title = dialogTitleInput.value;
-  let author = dialogAuthorInput.value;
-  let pages = dialogPagesInput.value;
-  let read = dialogReadChecked.value === "Yes" ? true : false;
+  let title = dialog.titleInput.value;
+  let author = dialog.authorInput.value;
+  let pages = dialog.pagesInput.value;
+  let read = dialog.readChecked.value === "Yes" ? true : false;
 
   myLibrary.push(new Book(title, author, pages, read));
   reloadCards();
@@ -82,11 +85,11 @@ function reloadCards() {
 }
 
 function clearDialogInputs() {
-  dialogTitleInput.value = '';
-  dialogAuthorInput.value = '';
-  dialogPagesInput.value = '';
-  dialogReadYes.checked = true;
-  dialogReadChecked = document.querySelector('input[name="read"]:checked');
+  dialog.titleInput.value = '';
+  dialog.authorInput.value = '';
+  dialog.pagesInput.value = '';
+  dialog.readYes.checked = true;
+  dialog.readChecked = document.querySelector('input[name="read"]:checked');
 }
 
 function createToggleRadioInput() {
@@ -133,10 +136,10 @@ function createToggleRadioInput() {
 }
 
 newBookButton.addEventListener('click', () => {
-  newBookDialog.showModal();
+  dialog.newBook.showModal();
 });
 
-addBookButton.addEventListener('click', e => {
+dialog.addBookButton.addEventListener('click', e => {
   if (document.querySelector("#new-book-dialog form").checkValidity()) {
     e.preventDefault();
     addBookToLibrary();
@@ -147,19 +150,19 @@ addBookButton.addEventListener('click', e => {
   
 });
 
-dialogCloseButton.addEventListener('click', () => {
+dialog.closeButton.addEventListener('click', () => {
   clearDialogInputs();
-  newBookDialog.close();
+  dialog.newBook.close();
 });
 
-dialogReadYes.addEventListener('click', () => {
-  dialogReadYes.checked = true;
-  dialogReadNo.checked = false;
-  dialogReadChecked = document.querySelector('input[name="read"]:checked');
+dialog.readYes.addEventListener('click', () => {
+  dialog.readYes.checked = true;
+  dialog.readNo.checked = false;
+  dialog.readChecked = document.querySelector('input[name="read"]:checked');
 });
 
-dialogReadNo.addEventListener('click', () => {
-  dialogReadYes.checked = false;
-  dialogReadNo.checked = true;
-  dialogReadChecked = document.querySelector('input[name="read"]:checked');
+dialog.readNo.addEventListener('click', () => {
+  dialog.readYes.checked = false;
+  dialog.readNo.checked = true;
+  dialog.readChecked = document.querySelector('input[name="read"]:checked');
 });
